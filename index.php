ad23a8dea6d1d6cd0058b96e305c392c525f6128
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-16 08:33:30 -0300

    Update index.html

--- index.php
+++ index.php
@@ -4,11 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>CRUD Vendas</title>
 </head>
 <body>
     <div class="container">
-        
+        <h1>CRUD - Sistema de Vendas</h1>
+        <hr>
+        <p> Operações de <b>Inserção</b>, <b>Leitura</b>, <b>Atualização</b> e <b>Exclusão</b> de dados. </p>
+
+        <ul>
+            <li><a href="">Fabricantes</a></li>
+            <li><a href="">Produtos</a></li>
+        </ul>
+
     </div>
     
 </body>
