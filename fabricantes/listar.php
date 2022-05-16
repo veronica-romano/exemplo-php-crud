76c7f4511a46c43e38cf0e7eb5597dd6ecf7457d
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-16 08:56:31 -0300

    ok

---
+++ fabricantes/listar.php
@@ -0,0 +1,36 @@
+<?php
+/*Script de conehão ao servidor banco de dados */
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "vendas";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Fabricantes | Select</h1>
+        <hr>
+        <h2>Lendo e carregando todos os fabricantes</h2>
+
+        <table>
+            <caption>Lista de fabricantes</caption>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
