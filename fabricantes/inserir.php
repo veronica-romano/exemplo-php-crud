e822b9685cd590ea86145b80bddbd75646fcc4a8
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-16 11:46:16 -0300

    inserir.php
    created and set

---
+++ fabricantes/inserir.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - Inserir</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Fabricantes | Inserir</h1>
+        <hr>
+        <form action="" method="post">
+        <div class="little-form-container">
+        <p>
+        <label for="nome"> Inserir Fabricante</label>
+        <input type="text" name="nome" id="nome" placeholder="Digite o nome do Fabricante ">
+        </p>
+        <button type="submit" name="inserir">
+            Inserir
+        </button>
+        </div>
+        </form>
+    </div>
+</body>
+</html>
+
+<?php
+
+?>
