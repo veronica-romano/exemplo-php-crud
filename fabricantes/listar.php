a4380fc179fe6de746f854aff47e56b94820e15e
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-16 09:23:26 -0300

    Update listar.php

--- fabricantes/listar.php
+++ fabricantes/listar.php
@@ -5,19 +5,15 @@ $usuario = "root";
 $senha = "";
 $banco = "vendas";
 
+try{
 //Criando a conexão com o MySQL (API/ Driver de conexão)
-
-$conexao = new PDO(
-    "mysql:host=$servidor;
-    dbname=$banco; charset=utf8",
-    $usuario, $senha
-    );
-
+    $conexao = new PDO("mysql:host=$servidor; dbname=$banco; charset=utf8", $usuario, $senha);
 // Habilita a  verificação de erros
-$conexao->setAttribute(
-    PDO::ATTR_ERRMODE, // constante de erros em geral 
-    PDO::ERRMODE_EXCEPTION // constande de exceção de erros
-); 
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+} catch(Exception $erro){
+    die("Erro: " .$erro->getMessage());
+}
+var_dump($conexao);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
