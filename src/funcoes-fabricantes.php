c06257a0668f14fc51073e162fdb2cefa11c0997
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-18 08:53:44 -0300

    function inserirFabricante

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -1,33 +1,28 @@
 <?php
-
 require_once "../src/conecta.php";
 // carregar os dados dos fabricantes
-
 function lerFabricantes(PDO $conexao):array{
-
         //string com comando sql
         $sql = "SELECT id, nome FROM fabricantes";
-
     try{
-            //preparação do comendo
+            //preparação do comando
             $consulta = $conexao->prepare($sql);
-    
             //execução do comando
-            $consulta->execute();
-        
+            $consulta->execute();   
             //capturar os resultados
-            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-            
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);       
     } catch (Exception $erro){
         die("Erro: ".$erro->getMessage());
     }
     return $resultado;
 }
-
 function inserirFabricante(PDO $conexao, string $nome){
     $sql = "INSERT INTO fabricantes(nome) VALUES ('$nome')";
     try {
-        
+        $consulta = $conexao->prepare($sql);
+        /*bindParam(':nomedoparametro', $variavelcomvalor, constantedeverificacao) */
+        $consulta->bindParam(':nome', $nome, PDO::PARAM_STR);
+        $consulta->execute();
     } catch (Exception $erro) {
         die("Erro: ".$erro->getMessage());
     }
