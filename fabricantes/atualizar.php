95c78ae103525f29a618101282fcab81a48f3799
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-20 08:30:12 -0300

    function
    atualizar fabricante

--- fabricantes/atualizar.php
+++ fabricantes/atualizar.php
@@ -3,7 +3,11 @@ require_once "../src/funcoes-fabricantes.php";
 //obtendo o valor do parâmetro da url
     $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
     $fabricante = lerUmFabricante($conexao, $id);
-
+    if (isset($_POST['atualizar'])) {
+        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+        atualizarFabricante($conexao, $id, $nome);
+        header("location:listar.php");
+    }
 ?> 
 <!DOCTYPE html>
 <html lang="pt-br">
