c17e3c4df3f69671934e60f9c9d8e079dae4219c
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-20 10:33:36 -0300

    js
    created and function set

--- fabricantes/script-listar.js
+++ fabricantes/script-listar.js
@@ -1,3 +1,13 @@
-function excluir() {
-    confirm("Press a button!\nEither OK or Cancel.");
-}
+const exclusao = document.querySelector('.exclusao');
+    exclusao.addEventListener('click', function(){
+        confirm("Tem certeza?\nAperte OK ou Cancel.");
+        let text;
+        if (confirm("Tem certeza?") == true) {
+            text = "OK!";
+        } else {
+            text = "Você cancelou!";
+        }
+    });
+        
+
+
