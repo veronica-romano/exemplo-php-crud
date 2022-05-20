e0734af92e4103c259c14499dd33905686aa9ebb
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-20 09:56:37 -0300

    Update excluir.php

--- fabricantes/excluir.php
+++ fabricantes/excluir.php
@@ -3,10 +3,7 @@ require_once "../src/funcoes-fabricantes.php";
 //obtendo o valor do parâmetro da url
     $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
     excluirFabricante($conexao, $id);
-    if (isset($_POST['excluir'])) {
-        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
-        excluirFabricante($conexao, $id);
-        header("location:listar.php");
-    }
+    header("location:listar.php");
+  
 
 
