6fa6c428e863c1afb78368a8df75bdbf5705c965
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-25 08:31:45 -0300

    produtos page created and set

---
+++ prudutos/listar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Produtos | Select</h1>
+        <hr>
+        <h2>Lendo e carregando todos os produto</h2>
+        <p><a src="/prudutos/listar.php">Inserir um novo produto</a></p>
+
+        <div class="produtos">
+            <article>
+                <h3>Nome do produto</h3>
+                <p>Fabricante</p>
+                <p>Preço</p>
+                <p>Quantidade</p>
+                <p>Descrição</p>
+            </article>
+        </div>
+    </div>
+</body>
+</html>
