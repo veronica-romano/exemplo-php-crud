d2b961c71994f2b9ecd868a893aa05911b310e08
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-06-27 08:35:37 -0300

    index changes
    adding oop descriptions

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>CRUD Vendas</title>
+    <title>CRUD Vendas (POO)</title>
 </head>
 <body>
     <div class="container">
-        <h1>CRUD - Sistema de Vendas</h1>
+        <h1>CRUD - Sistema de Vendas (Versão POO)</h1>
         <hr>
-        <p> Operações de <b>Inserção</b>, <b>Leitura</b>, <b>Atualização</b> e <b>Exclusão</b> de dados. </p>
+        <p> Operações de <b>Inserção</b>, <b>Leitura</b>, <b>Atualização</b> e <b>Exclusão</b> de dados usando namespaces, composer, autoload, classes, métodos e propriedades.</p>
 
         <ul>
             <li><a href="fabricantes/listar.php">Fabricantes</a></li>
