8b6323c3a46f30a593d7e16f0da7fe00bb392253
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-06-27 09:27:55 -0300

    connection and json

---
+++ src/Banco.php
@@ -0,0 +1,36 @@
+<?php
+namespace CrudPoo;
+use PDO, Exception;
+abstract class Banco{
+
+    private static string $servidor = "localhost";
+    private static string $usuario = "root";
+    private static string $senha = "";
+    private static string $banco = "vendas";
+    private static PDO $conexao;
+
+    public static function conecta():PDO{
+        try {
+           self::$conexao = new PDO(
+            "mysql:host=".self::$servidor.";
+            dbname=".self::$banco.";
+            charset=utf8",
+            self::$usuario,
+            self::$senha,
+           );
+           self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           echo "ok";
+           
+        } catch (Exception $erro) {
+           die("Deu ruim: " . $erro->getMessage());
+        }
+        return self::$conexao;
+    }
+}
+
+
+Banco::conecta();
+
+
+
+?>
