cfbffdc34c63bb1fbca61a90bf6da33d81774911
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-06-29 09:45:22 -0300

    Update excluir.php

--- fabricantes/excluir.php
+++ fabricantes/excluir.php
@@ -1,12 +1,10 @@
 <?php
 require_once "../vendor/autoload.php";
 use CrudPoo\Fabricante;
-
-require_once "../src/funcoes-fabricantes.php";
 //obtendo o valor do parâmetro da url
     $fabricante = new Fabricante;
-    $fabricante->setId(filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT));
-    $delFabricante = $fabricante->excluirFabricante();
+    $fabricante->setId($_GET['id']);
+    $fabricante->excluirFabricante();
     header("location:listar.php");
   
 
