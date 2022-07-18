23019b5aaef1cf142e81e0e1e17dc7c799a91551
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-07-18 09:00:59 -0300

    removing bootstrap via composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '73a3be718d8b3f6d76cd693846e4441681b9946a',
+        'reference' => '1348a07de048204345eae98bd850a8a59f021a94',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,26 +13,11 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '73a3be718d8b3f6d76cd693846e4441681b9946a',
+            'reference' => '1348a07de048204345eae98bd850a8a59f021a94',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
-        ),
-        'twbs/bootstrap' => array(
-            'pretty_version' => 'v5.2.0-beta1',
-            'version' => '5.2.0.0-beta1',
-            'reference' => 'fca04c07131a81e625a516cf7b98a8c7df0df1c1',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../twbs/bootstrap',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'twitter/bootstrap' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => 'v5.2.0-beta1',
-            ),
         ),
     ),
 );
