2d8461e1b070aac8be81424a9e3b676c8967a9af
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-07-18 08:42:18 -0300

    packagist

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '73a3be718d8b3f6d76cd693846e4441681b9946a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '73a3be718d8b3f6d76cd693846e4441681b9946a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.2.0-beta1',
+            'version' => '5.2.0.0-beta1',
+            'reference' => 'fca04c07131a81e625a516cf7b98a8c7df0df1c1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.2.0-beta1',
+            ),
+        ),
+    ),
+);
