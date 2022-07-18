1b598b66f6e15ebec60751b1f160d3e790a695ac
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-07-18 10:54:34 -0300

    heredoc PHP

--- exportarpdf.php
+++ exportarpdf.php
@@ -1,11 +1,22 @@
 <?php
 
-use Diversos\Utilitarios;
+use Masterminds\HTML5;
 
 require_once "vendor/autoload.php";
-
 session_start();
+foreach ($_SESSION['dados'] as $fabricante) {
+    echo $fabricante['nome']."<br>";
+}
+// sintaxe heredoc php
+$data = date("d/m/Y");
+$conteudo = <<<HTML
+    <div style="border: solid 2px; padding: 10px; width:70%; margin: 0 auto; text-align: center">
+        <h2>Resumo de fabricantes - $data</h2>
+    </div>
 
-Utilitarios::teste($_SESSION['dados']);
+HTML;
 
+echo $conteudo;
 ?>
+
+
